7944edbcbcab2c93afe3ccc0e1cc2254d44e4c0f
Agaricus <agaricusb@yahoo.com>
2013-05-01 20:00:43 -0700

    Get coremod location from injected data

--- src/main/java/agaricus/mods/Jarmod2Coremod/Scanner.java
+++ src/main/java/agaricus/mods/Jarmod2Coremod/Scanner.java
@@ -2,20 +2,29 @@ package agaricus.mods.Jarmod2Coremod;
 
 import cpw.mods.fml.relauncher.IFMLCallHook;
 
+import java.io.File;
 import java.util.Map;
 
 public class Scanner implements IFMLCallHook {
+
+    public File coremodLocation;
 
     /*
      * Gives the mod coremod data if it wants it.
      */
     @Override
     public void injectData(Map<String, Object> data) {
+        if (data.containsKey("coremodLocation")) {
+            coremodLocation = (File) data.get("coremodLocation");
+        }
     }
 
     @Override
     public Void call() throws Exception {
         System.out.println("PRELOADING XXX");
+        System.out.println("coremodLocation = "+coremodLocation);
+
+        System.exit(0); // for testing
         return null;
     }
 }
